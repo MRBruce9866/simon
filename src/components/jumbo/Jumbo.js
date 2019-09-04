1bdee093a7b8c881e75308b066d76835ad6d2523
Mark <mrbruce9866@gmail.com>
2019-09-04 02:13:35 -0400

    fixed a typo in the jumbotron

--- src/components/jumbo/Jumbo.js
+++ src/components/jumbo/Jumbo.js
@@ -6,7 +6,7 @@ function Jumbo() {
   return (
     <div className="jumbotron text-center mb-0 jumbo pb-3">
         <h1 class="display-3 pb-5">SIMON</h1>
-        <p class="lead">See if you can keep up with simon by pressing the white buttonn. You know? The one that's flashing.</p>
+        <p class="lead">See if you can keep up with simon by pressing the white button. You know? The one that's flashing.</p>
     </div>
   )
 }
